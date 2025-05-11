5759eca0182b88eab24c0ccfa58b7232e5927e04
hellochaeyoung <cyahn0831@hansung.ac.kr>
2025-05-11 20:18:04 +0900

    custom 레포지토리 인터페이스화, 일반 레포지토리 상속받도록 수정

--- ecommerce/src/main/java/com/hello/ecommerce/repository/ProductsRepository.java
+++ ecommerce/src/main/java/com/hello/ecommerce/repository/ProductsRepository.java
@@ -1,13 +1,14 @@
 package com.hello.ecommerce.repository;
 
 import com.hello.ecommerce.entity.Products;
+import com.hello.ecommerce.repository.custom.ProductCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductsRepository extends JpaRepository<Products, Long> {
+public interface ProductsRepository extends JpaRepository<Products, Long>, ProductCustomRepository {
 
     List<Products> findAllByOrderByCreatedAtDesc();
 }
---
+++ ecommerce/src/main/java/com/hello/ecommerce/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.hello.ecommerce.repository;
+
+import com.hello.ecommerce.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
